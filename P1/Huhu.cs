3fe53dd05bd27cd08139bc3b3e347f68bc80125a
JayBee12345 <jay.bee3286@gmail.com>
2020-04-14 15:49:43 +0200

    neuer commit

---
+++ P1/Huhu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1
+{
+    class Huhu{
+
+        private String name = "Gustav";
+        private int alter = 29;
+
+        public Huhu()
+        {
+
+        }
+    
+        public String ausgabe()
+           {
+            return name + "\n" + alter;
+        }
+    
+    }
+}
--- P1/Program.cs
+++ P1/Program.cs
@@ -6,7 +6,10 @@ namespace P1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Huhu test = new Huhu();
+            test.ausgabe();
+           
         }
     }
 }
